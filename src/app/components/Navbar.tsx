b6dc0c67167a8fcf0ab86df270299658f7680ce7
Max <mix@Maxs-MacBook-Pro.local>
2023-11-14 02:01:42 -0500

    Add prettier and weather headline

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="mx-auto flex max-w-7xl flex-wrap items-center justify-between bg-rose-400 p-6">
+      <div className="mr-6 flex flex-shrink-0 items-center text-white">
+        <Link href="/">
+          <span className="text-xl font-semibold tracking-tight">Weather</span>
+        </Link>
+      </div>
+    </nav>
+  );
+}
