df34aca6dd70ff67ad4474a825a27fe57d8d7170
Max <max@maxsu.dev>
2023-11-15 01:45:10 -0500

    css bg to black changes, extend navbar to 8xl

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="mx-auto flex max-w-7xl flex-wrap items-center justify-between bg-rose-400 p-6">
+    <nav className="mx-auto flex max-w-8xl flex-wrap items-center justify-between bg-rose-400 p-6">
       <div className="mr-6 flex flex-shrink-0 items-center text-white">
         <Link href="/">
           <span className="text-xl font-semibold tracking-tight">Weather</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import SunnyDay from "../assets/day.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-inherit p-6">
       <div className="mx-auto flex max-w-sm items-center rounded-xl bg-black p-6 shadow-lg">
         <Image src={SunnyDay} width={128} height={128} alt="Sunny Day" />
         <div className="-ml-5">
           <div className="text-5xl font-medium text-white">71°F</div>
-          <div className="text-xs font-medium text-white">Feels like 71°F</div>
+          <div className="text-sm font-medium text-white">Feels 71°F</div>
         </div>
       </div>
     </main>
