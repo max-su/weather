8906ee228e34a1e18d80f1aa25ac34dcaf6f2499
Max <max@maxsu.dev>
2023-11-16 02:48:20 -0500

    moved current weather headline to a component, modularized page.tsx

---
+++ src/app/components/WeatherHeadline.tsx
@@ -0,0 +1,58 @@
+import internal from "stream";
+import Image from "next/image";
+import SunnyIcon from "../../assets/day.svg";
+import CloudyIcon from "../../assets/cloudy.svg";
+import SnowyIcon from "../../assets/snowy-1.svg";
+import RainyIcon from '../../assets/rainy-1.svg';
+import React from "react";
+
+interface Props {
+  wmoCode: number
+  temperature: number
+  apparentTemperature: number
+}
+export const WeatherHeadline: React.FC<Props> = ({wmoCode, temperature, apparentTemperature}) => {
+  // 0 -> 2 Sunny
+  // 3 -> 20 Cloudy
+  // 20 -> 29 Rain just before, can call cloudy?
+  // 30 -> 35 Sandstorm
+  // 36 -> 39 Snow
+  // 40 -> 49 Fog
+  // 50 -> 59 Drizzle
+  // 60 -> 69 Rainy
+  // 70 -> 79 Snow
+  // 80 -> 99 Rain
+  // https://www.nodc.noaa.gov/archive/arc0021/0002199/1.1/data/0-data/HTML/WMO-CODE/WMO4677.HTM
+  let imgIcon = null;
+  if (0 <= wmoCode && wmoCode <= 2) {
+    imgIcon = <Image src={SunnyIcon} width={128} height={128} alt="Sunny Day" />;
+  }
+  else if (3 <= wmoCode && wmoCode <= 29) {
+    imgIcon = <Image src={CloudyIcon} width={128} height={128} alt="Cloudy Day" />;
+  }
+  else if (30 <= wmoCode && wmoCode <= 35) {
+    imgIcon = <Image src={"https://worldweather.wmo.int/images/1.png"} width={128} height={128} alt="Sandstorm" />;
+  }
+  else if ((36 <= wmoCode && wmoCode <= 39) || (70 <= wmoCode && wmoCode <= 79)) {
+    imgIcon = <Image src={SnowyIcon} width={128} height={128} alt="Snowy Day" />;
+  }
+  else if (40 <= wmoCode && wmoCode <= 49) {
+    imgIcon = <Image src={"https://worldweather.wmo.int/images/16.png"} width={128} height={128} alt="Foggy Day" />;
+  }
+  else if ((50 <= wmoCode && wmoCode <= 69) || (80 <= wmoCode && wmoCode <= 99)) {
+    imgIcon = <Image src={RainyIcon} width={128} height={128} alt="Rainy Day" />
+  }
+  else if (70 <= wmoCode && wmoCode <= 79) {
+    imgIcon = <Image src={SnowyDay} width={128} height={128} alt="Snowy Day" />
+  }
+
+  return (
+    <div className="mx-auto flex max-w-sm items-center bg-black p-6 shadow-lg">
+      {imgIcon}
+      <div className="-ml-5">
+        <div className="text-5xl font-medium text-white">{temperature}°F</div>
+        <div className="text-sm font-medium text-white">Feels {apparentTemperature}°F</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import Image from "next/image";
 import {useState} from 'react';
-import SunnyDay from "../assets/day.svg";
+import useSWR from 'swr';
+import { WeatherHeadline } from './components/WeatherHeadline';
+
 
 export default function Home() {
+  // Geolocation
   const [longitude, setLongitude] = useState<number | null>(null)
   const [latitude, setLatitude] = useState<number | null>(null)
   
@@ -18,6 +20,11 @@ export default function Home() {
     })
   };
 
+  // SWR 
+  const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+  const { data, error, isLoading } = useSWR('https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&temperature_unit=fahrenheit&hourly=temperature_2m,dew_point_2m,apparent_temperature&timezone=auto', fetcher)
+
   if (longitude === null || latitude === null) {
     return (
       <main className="flex min-h-screen flex-col items-center bg-inherit p-6">
@@ -27,16 +34,22 @@ export default function Home() {
       </main>
     );
   }
+  else if (error) {
+    return (
+      <p>Failed to load</p>
+    );
+  }
+  else if (isLoading) {
+    return (
+      <main className="flex min-h-screen flex-col items-center bg-inherit p-6">
+        <svg className="anime-spin h-10, w-10 mr-3"></svg>
+      </main>
+    );
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-inherit p-6">
-      <div className="mx-auto flex max-w-sm items-center bg-black p-6 shadow-lg">
-        <Image src={SunnyDay} width={128} height={128} alt="Sunny Day" />
-        <div className="-ml-5">
-          <div className="text-5xl font-medium text-white">71°F</div>
-          <div className="text-sm font-medium text-white">Feels 71°F</div>
-        </div>
-      </div>
+      <WeatherHeadline wmoCode={71} temperature={71} apparentTemperature={71} />
       <div className="mx-auto flex max-w-sm items-center bg-black p-6 shadow-lg">
         <p className="text-white">Longitude: {longitude}, Latitude: {latitude}</p>
       </div>
